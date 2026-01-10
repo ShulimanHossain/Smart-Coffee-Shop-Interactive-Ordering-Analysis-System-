49d01b92aaa17762b4308c5e7d7cbf8d0b07fc1d
ShulimanHossain <shulimansh@gmail.com>
2026-01-11 00:27:10 +0600

    Completed

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 class Config:
-           MYSQL_HOST = 
-           MYSQL_PORT =       
-           MYSQL_USER =           
-           MYSQL_PASSWORD =      
-           MYSQL_DB = 
+    # MySQL server address (use 'localhost' if database is on the same machine)
+    MYSQL_HOST = 'localhost'
+
+    # MySQL server port (default is 3306)
+    MYSQL_PORT = 3306
+
+    # MySQL username (e.g., 'root' or a custom database user)
+    MYSQL_USER = 'your_mysql_username'
+
+    # MySQL password for the above user
+    MYSQL_PASSWORD = 'your_mysql_password'
+
+    # Name of the database used by the application
+    MYSQL_DB = 'your_database_name'
